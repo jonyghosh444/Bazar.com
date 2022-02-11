cf2aa941f1a998741d34ed8030873e2ab373f093
Jony <16101045@uap-bd.edu>
2022-02-11 20:51:43 +0600

    ugraded

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
 
+# Create your views here.
 def Index(request):
     return render(request, 'shop/index.html')
 
