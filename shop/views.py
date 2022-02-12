fa3198364962e18084d54a449b1ba2c6af37fcd8
Jony <16101045@uap-bd.edu>
2022-02-12 15:53:18 +0600

    Added Image function

--- shop/views.py
+++ shop/views.py
@@ -3,23 +3,31 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+
+
 def Index(request):
     return render(request, 'shop/index.html')
+
 
 def About(request):
     return render(request, 'shop/About.html')
 
+
 def Contact(request):
     return render(request, 'shop/Contact.html')
+
 
 def Tracker(request):
     return render(request, 'shop/Tracker.html')
 
+
 def Search(request):
     return render(request, 'shop/Search.html')
 
+
 def Productview(request):
     return render(request, 'shop/Productview.html')
+
 
 def Checkout(request):
     return render(request, 'shop/Checkout.html')
