054c94a5a30fec2559a9a16644c9c7c2cc39b414
Jony <16101045@uap-bd.edu>
2022-02-12 23:01:19 +0600

    Showing product on homepage

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from .models import Product
+from math import ceil
 # Create your views here.
 
 
 def Index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    print(products)
+    n = len(products)
+    no_of_slides = n//4+ceil(n/4-n//4)
+    params = {'no_of_slides': no_of_slides, 'range': range(1,no_of_slides), 'product': 'products'}
+    return render(request, 'shop/index.html', params)
 
 
 def About(request):
