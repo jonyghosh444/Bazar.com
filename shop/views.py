71285fbb408dedef78505395092b9f6886a5b1f6
Jony <16101045@uap-bd.edu>
2022-02-13 22:48:29 +0600

    Added Fetching product data by category function in shop homepage

--- shop/views.py
+++ shop/views.py
@@ -6,11 +6,21 @@ from math import ceil
 
 
 def Index(request):
-    products = Product.objects.all()
-    n = len(products)
-    nSlides = n//6 + ceil((n/6)-(n//6))
-    allProds = [[products, range(1, nSlides), nSlides],
-                [products, range(1, nSlides), nSlides]]
+    # products = Product.objects.all()
+    # n = len(products)
+    # nSlides = n//6 + ceil((n/6)-(n//6))
+    # allProds = [[products, range(1, nSlides), nSlides],
+    #             [products, range(1, nSlides), nSlides]]
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n//6 + ceil((n/6) - (n//6))
+        allProds.append([prod, range(1, nSlides), nSlides])
+
+
     params = {'allProds': allProds}
     # params = {'no_of_slides': nSlides, 'range': range(1, nSlides), 'product': products}
     return render(request, 'shop/index.html', params)
